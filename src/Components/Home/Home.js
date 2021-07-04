07cd5f3ad2b81433eaba3862536bf646cdefe2a0
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-07-05 01:05:57 +0600

    home and header comoponets added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import Header from "../Header/Header";
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h2>this is home page </h2>
+    </div>
+  );
+};
+
+export default Home;
