8e0e974d2e26511a84c4530e8b3af12b4d691ca2
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-07-06 13:29:48 +0600

    search box is working with enter key

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,20 +3,32 @@ import "./Home.css";
 import Header from "../Header/Header";
 import { InputGroup, FormControl, Button } from "react-bootstrap";
 const Home = () => {
+  const handleSearchBtn = () => {
+    const searchedText = document.getElementById("searchInputField").value;
+    console.log(searchedText);
+  };
+
   return (
     <div className="container">
-      <div className="row">
+      <div className="">
         <Header />
       </div>
-      <div className=" ml-5 pl-5">
-        <div className="row mt-5 ml-5 pl-5">
+      <div className="ml-5 pl-5">
+        <div className="mt-5 ml-5 pl-5">
           <InputGroup size="lg" className="mb-3 w-50 ml-5 pl-5">
             <FormControl
-              className="ml-5 pl-5"
+              id="searchInputField"
               placeholder="Search Items"
               aria-label="searchInput"
+              onKeyUp={(event) => {
+                if (event.keyCode === 13) {
+                  handleSearchBtn();
+                }
+              }}
             />
-            <Button className="customColor ">Search</Button>
+            <Button onClick={handleSearchBtn} className="customColor ">
+              Search
+            </Button>
           </InputGroup>
         </div>
       </div>
