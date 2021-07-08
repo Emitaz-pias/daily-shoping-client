9749f3ac8df577661c06a50425e4aacf104071c2
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-07-08 12:53:26 +0600

    hr line added before the total div

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -9,15 +9,30 @@ const Orders = () => {
       <h2 className="m-5" style={{ color: "#363958" }}>
         Checkout
       </h2>
-      <div className="detailsContainer  d-flex justify-content-between">
-        <div className="descriptionContainer">
-          {" "}
-          <h6>Description</h6>
-        </div>
-        <div className="qtyPriceContainer w-25 d-flex justify-content-between">
-          <h6>Quantity</h6>
 
-          <h6 className="pl-5">Price</h6>
+      <div className="shadow ml-5 p-3 mb-5 bg-body rounded">
+        <div className="detailsContainer pl-2 pr-3  d-flex justify-content-between">
+          <div className="descriptionContainer">
+            {" "}
+            <h6>Description</h6>
+            <p>aklsdjflkajsdflakjsdflkjakldsfj</p>
+            <p>aklsdjflkajsdflakjsdflkjakldsfj</p>
+            <p>aklsdjflkajsdflakjsdflkjakldsfj</p>
+            <p>aklsdjflkajsdflakjsdflkjakldsfj</p>
+            <p>aklsdjflkajsdflakjsdflkjakldsfj</p>
+            <p>aklsdjflkajsdflakjsdflkjakldsfj</p>
+            <p>aklsdjflkajsdflakjsdflkjakldsfj</p>
+            <p>aklsdjflkajsdflakjsdflkjakldsfj</p>
+          </div>
+          <div className="qtyPriceContainer ml-5 pl-5 w-50 d-flex justify-content-between">
+            <h6>Quantity</h6>
+
+            <h6 className="pl-5">Price</h6>
+          </div>
+        </div>
+        <div className="calculationDiv">
+          <hr className="bg-secondary.bg-gradient w-100" />
+          <h6>Total</h6>
         </div>
       </div>
     </Container>
