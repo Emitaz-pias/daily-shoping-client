5dcc29439cecc26160d083d790ee7a182c256c90
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-07-10 05:55:56 +0600

    react hook form added

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,21 +1,38 @@
 import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
+import { useForm } from "react-hook-form";
+
 const Admin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div style={{ display: "inline-flex" }}>
       <Sidebar></Sidebar>
       <div id="manageProducts">
-        <h1>hi this is add product</h1>
-        <p>details </p>
-        <p>details </p>
-        <p>details </p>
-        <p>details </p>
-        <p>details </p>
-        <p>details </p>
-        <p>details </p>
-        <p>details </p>
-        <p>details </p>
-        <p>details </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            placeholder="Product Name"
+            {...register("Product Name", { required: true, maxLength: 80 })}
+          />
+          <input
+            type="text"
+            placeholder="Weight"
+            {...register("Weight", { required: true, maxLength: 100 })}
+          />
+          <input
+            type="number"
+            placeholder="Price"
+            {...register("Price", { required: true, maxLength: 12 })}
+          />
+
+          <input type="submit" />
+        </form>
       </div>
       <div id="addProduct">
         <h1>manage product</h1>
