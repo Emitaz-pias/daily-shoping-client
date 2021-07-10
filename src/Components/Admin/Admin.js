1fccce4bd9e19c953abefcf287bb5013219bb588
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-07-10 06:09:14 +0600

    shadow added to the h1 and to the input field

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -14,25 +14,34 @@ const Admin = () => {
     <div style={{ display: "inline-flex" }}>
       <Sidebar></Sidebar>
       <div id="manageProducts">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            placeholder="Product Name"
-            {...register("Product Name", { required: true, maxLength: 80 })}
-          />
-          <input
-            type="text"
-            placeholder="Weight"
-            {...register("Weight", { required: true, maxLength: 100 })}
-          />
-          <input
-            type="number"
-            placeholder="Price"
-            {...register("Price", { required: true, maxLength: 12 })}
-          />
+        <div className=" mt-2 p-3">
+          <div className="shadow  p-1 pl-2  mb-5 bg-body rounded ">
+            <h2>Add Product</h2>
+          </div>
+          <div className="shadow pt-5 m-5 p-5 mb-5 bg-body rounded">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="text"
+                placeholder="Product Name"
+                {...register("Product Name", { required: true, maxLength: 80 })}
+              />
+              <br />
+              <input
+                type="text"
+                placeholder="Weight"
+                {...register("Weight", { required: true, maxLength: 100 })}
+              />
 
-          <input type="submit" />
-        </form>
+              <input
+                type="text"
+                placeholder="Price"
+                {...register("Price", { required: true, maxLength: 12 })}
+              />
+              <input type="file" placeholder="Upload" />
+              <input type="submit" />
+            </form>
+          </div>
+        </div>
       </div>
       <div id="addProduct">
         <h1>manage product</h1>
