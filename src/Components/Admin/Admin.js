0d04ff62fa207675af04e94758fc33561169389e
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-07-10 07:08:16 +0600

    form sketch done

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -11,43 +11,66 @@ const Admin = () => {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div style={{ display: "inline-flex" }}>
-      <Sidebar></Sidebar>
-      <div id="manageProducts">
-        <div className=" mt-2 p-3">
-          <div className="shadow  p-1 pl-2  mb-5 bg-body rounded ">
-            <h2>Add Product</h2>
+    <div>
+      <div className="row w-100">
+        <div className="col-3">
+          <Sidebar></Sidebar>
+        </div>
+        {/* <div id="manageProducts">
+        <h1>manage product</h1>
+      </div> */}
+        <div className="col" id="addProduct">
+          <div className="shadow mt-2  p-3 mb-5 bg-body rounded">
+            <h2 style={{ fontWeight: 700 }}>Add product</h2>
           </div>
-          <div className="shadow pt-5 m-5 p-5 mb-5 bg-body rounded">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <input
-                type="text"
-                placeholder="Product Name"
-                {...register("Product Name", { required: true, maxLength: 80 })}
-              />
-              <br />
-              <input
-                type="text"
-                placeholder="Weight"
-                {...register("Weight", { required: true, maxLength: 100 })}
-              />
-
-              <input
-                type="text"
-                placeholder="Price"
-                {...register("Price", { required: true, maxLength: 12 })}
-              />
-              <input type="file" placeholder="Upload" />
-              <input type="submit" />
+          <div className="shadow">
+            <form action="">
+              <div className="row">
+                <div className="col">
+                  <h6>Add Product Name</h6>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name=""
+                    placeholder="Enter product name"
+                    id=""
+                  />
+                </div>
+                <div className="col">
+                  <h6>Weight</h6>
+                  <input
+                    type="text"
+                    name=""
+                    placeholder="Weight"
+                    className="form-control"
+                    id=""
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <h6>Add Price</h6>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name=""
+                    placeholder="Enter price"
+                    id=""
+                  />
+                </div>
+                <div className="col">
+                  <h6> Add Image</h6>
+                  <input type="file" name="" placeholder="Upload file" id="" />
+                </div>
+              </div>
             </form>
           </div>
         </div>
-      </div>
-      <div id="addProduct">
-        <h1>manage product</h1>
       </div>
     </div>
   );
 };
 
 export default Admin;
+
+// style={{ display: "inline-flex" }}
